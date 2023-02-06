144ca0e06f4785eff8b6fc4453bfed1fffa280bc
ikiyuuji <85266541+ikiyuuji@users.noreply.github.com>
2023-02-06 16:11:50 -0300

    Create Teste Com Lista de Variaveis.py

---
+++ Teste Com Lista de Variaveis.py
@@ -0,0 +1,19 @@
+  
+grupo = ["Pedro", "Kaiki", "Vitor","Atila"]
+print(grupo)
+SN=(input("Deseja adicionar participante? "))
+
+while SN == "sim":
+    add_part=(input("Digite o nome de um novo participante: "))
+
+    grupo.extend([f"{add_part}"])
+
+    print(grupo)
+   
+    SN=(input("Deseja adicionar participante? "))
+
+    if SN == "nao" or SN == "não":
+        
+        print(f'Seu Grupo é composto por: {grupo}')
+
+        break
